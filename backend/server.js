e33c6276ecd53f3adedc18d43de67c7a8b43a0e0
hank.dohwankim <hank.dohwankim@gmail.com>
2020-10-04 11:10:46 -0500

    cart reducer, action added

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,6 @@ connectDB();
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Hello');
   next();
 });
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,11 +5,17 @@ import {
   productListReducer,
   productDetailsReducer,
 } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
+
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage).getItem('cartItems')
+  : [];
 
 const initialState = {};
 
